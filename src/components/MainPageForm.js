9e647d7f3eb296bd418674ab64fedd8ba0589283
Ofek Shiffman <ofeks10@gmail.com>
2020-03-27 11:08:24 -0700

    Updated yup import

--- src/components/MainPageForm.js
+++ src/components/MainPageForm.js
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 import { Formik } from 'formik';
-let yup = require('yup');
+import * as yup from 'yup'
 
 
 const schema = yup.object({
