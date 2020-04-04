8bcaebfb387f5e334376159aaa1a0201dab38973
Ofek Shiffman <ofeks10@gmail.com>
2020-04-04 13:24:41 -0700

    Basic layout somewhat

--- src/components/MainContent/components/CalendarContent/components/CalendarContent.js
+++ src/components/MainContent/components/CalendarContent/components/CalendarContent.js
@@ -1,11 +1,10 @@
 import React, {useState, useEffect} from 'react'
 
 import {
-    Card, 
-    ListGroup,
     Container,
     Row,
-    Col
+    Col,
+    Table
 } from 'react-bootstrap'
 
 import './CalendarContent.css'
@@ -39,11 +38,16 @@ function CalendarContent(props) {
         getData(props.match.params.hash)
     }, [props.match.params.hash])
 
-    let rows = [];
-    for (let i = 1; i <= 48; i++) {
-        rows.push(<ListGroup.Item variant='dark'>{i}</ListGroup.Item>)
+    let rows = []
+    let rows2 = []
+
+    for (let i = 0; i < 48; i++) {
+        rows.push(<tr><td onClick={() => {alert(i)}}>{i}</td><td>{i}</td><td>{i}</td></tr>)
     }
 
+    for (let i = 1; i < 36; i+=7) {
+        rows2.push(<tr><td>{i}</td><td>{i+1}</td><td>{i+2}</td><td>{i+3}</td><td>{i+4}</td><td>{i+5}</td><td>{i+6}</td></tr>);
+    }
 
     return (
         <Container fluid className="main-content">
@@ -52,57 +56,33 @@ function CalendarContent(props) {
                     <h2>{isCalendarTitleLoading ? "Loading..." : calendarTitle}</h2>
                 </Col>
             </Row>
-            <Row styleName='m-0' noGutters>
-                <Col styleName='text-left'>Test</Col>
-                <Col>
-                    <Card bg='dark'>
-                        <Card.Header>
-                            test
-                        </Card.Header>
-                        <ListGroup variant='dark'>
-                            {rows}
-                        </ListGroup>
-                    </Card>
+            <Row>
+                <Col lg={3}>
+                    <Table variant='dark' style={{width: '100%'}} bordered>
+                        <tbody>
+                            {rows2}
+                        </tbody>
+                    </Table>
                 </Col>
-                <Col>
-                    <Card bg='dark'>
-                        <Card.Header>
-                            test
-                        </Card.Header>
-                        <ListGroup variant='dark'>
+                <Col lg={9}>
+                    <Table bordered variant='dark'>
+                        <thead>
+                            <tr>
+                                <th>
+                                    04.04.2020
+                                </th>
+                                <th>
+                                    05.04.2020
+                                </th>
+                                <th>
+                                    06.04.2020
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
                             {rows}
-                        </ListGroup>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card bg='dark'>
-                        <Card.Header>
-                            test
-                        </Card.Header>
-                        <ListGroup variant='dark'>
-                            {rows}
-                        </ListGroup>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card bg='dark'>
-                        <Card.Header>
-                            test
-                        </Card.Header>
-                        <ListGroup variant='dark'>
-                            {rows}
-                        </ListGroup>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card bg='dark'>
-                        <Card.Header>
-                            test
-                        </Card.Header>
-                        <ListGroup variant='dark'>
-                            {rows}
-                        </ListGroup>
-                    </Card>
+                        </tbody>
+                    </Table>
                 </Col>
             </Row>
         </Container>
